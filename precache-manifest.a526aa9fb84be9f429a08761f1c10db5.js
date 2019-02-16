3e020a012e27d4d2171702adca02aeb9b57a19f0
srinivasan1995 <r.srinivasan1995@gmail.com>
2019-02-16 09:06:38 +0530

    Updates

---
+++ precache-manifest.a526aa9fb84be9f429a08761f1c10db5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "693f46ea7f34a4904249",
+    "url": "/Ball-through-hoop/static/js/runtime~main.693f46ea.js"
+  },
+  {
+    "revision": "980a952bca20935bf27f",
+    "url": "/Ball-through-hoop/static/js/main.980a952b.chunk.js"
+  },
+  {
+    "revision": "0cbaa49166ba21d9c7d2",
+    "url": "/Ball-through-hoop/static/js/1.0cbaa491.chunk.js"
+  },
+  {
+    "revision": "980a952bca20935bf27f",
+    "url": "/Ball-through-hoop/static/css/main.00b3a12a.chunk.css"
+  },
+  {
+    "revision": "4a279a6aec0cb7d780a21c2cbc2246c2",
+    "url": "/Ball-through-hoop/index.html"
+  }
+];
